04b40d67e22cd2a3071aeae02b628de15d75ac7d
Szabó Ármin András <46499638+Lonelymous@users.noreply.github.com>
2021-10-13 13:51:09 +0200

    programming basic beta up

---
+++ routes/programming/script.js
@@ -0,0 +1,18 @@
+let mainNavLinks = document.querySelectorAll("main nav ul li a");
+let mainSections = document.querySelectorAll("main article section");
+
+let lastId;
+let cur = [];
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+    if (section.offsetTop <= fromTop && section.offsetTop + section.offsetHeight > fromTop) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
